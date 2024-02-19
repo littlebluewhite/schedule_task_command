15104e21e70356e8e99597edfa64edfe2f683b2a
Wilson <wwilson008@gmail.com>
2024-02-19 17:10:26 +0800

    version 2.13.1

--- docs/docs.go
+++ docs/docs.go
@@ -2938,7 +2938,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "2.13.0",
+	Version:          "2.13.1",
 	Host:             "127.0.0.1:5487",
 	BasePath:         "",
 	Schemes:          []string{},
