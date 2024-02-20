2aef2d27774237e232b37e58692ce8824848563a
Wilson <wwilson008@gmail.com>
2024-02-20 11:08:14 +0800

    version 2.13.2

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -37,7 +37,7 @@ func init() {
 }
 
 // @title           Schedule-Task-Command swagger API
-// @version         2.13.1
+// @version         2.13.2
 // @description     This is a schedule-command server.
 // @termsOfService  http://swagger.io/terms/
 
