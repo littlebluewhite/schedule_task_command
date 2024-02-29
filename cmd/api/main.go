deeff97bb4b32334548a44a8de967c3888d5fbad
Wilson <wwilson008@gmail.com>
2024-02-29 18:57:32 +0800

    docker-compose.yaml[update] redis connect[update] version 2.13.5

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -37,7 +37,7 @@ func init() {
 }
 
 // @title           Schedule-Task-Command swagger API
-// @version         2.13.4
+// @version         2.13.5
 // @description     This is a schedule-command server.
 // @termsOfService  http://swagger.io/terms/
 
