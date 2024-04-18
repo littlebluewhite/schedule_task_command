fb330a809e662aee638d8fd528e57a3322052eb1
Wilson <wwilson008@gmail.com>
2024-04-18 11:52:24 +0800

    check com response data is json[debug] version 2.13.14

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -37,7 +37,7 @@ func init() {
 }
 
 // @title           Schedule-Task-Command swagger API
-// @version         2.13.13
+// @version         2.13.14
 // @description     This is a schedule-command server.
 // @termsOfService  http://swagger.io/terms/
 
