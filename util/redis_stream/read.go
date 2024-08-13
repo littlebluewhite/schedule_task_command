3366f799575590f51af5d1f7c4eca905988f208e
Wilson <wwilson008@gmail.com>
2024-08-13 11:11:24 +0800

    @@ redis stream[modify]

--- util/redis_stream/read.go
+++ util/redis_stream/read.go
@@ -2,6 +2,7 @@ package redis_stream
 
 import (
 	"context"
+	"fmt"
 	"github.com/goccy/go-json"
 	"github.com/redis/go-redis/v9"
 	"schedule_task_command/api"
@@ -54,18 +55,9 @@ func (rs *RedisStream) Start(ctx context.Context, streamComMap map[string]func(m
 }
 
 func (rs *RedisStream) streamInit(ctx context.Context) (err error) {
-	if e := rs.rdb.XInfoGroups(ctx, rs.streamName).Err(); e != nil {
-		r, _ := rs.rdb.XAdd(ctx, &redis.XAddArgs{
-			Stream: rs.streamName,
-			Values: map[string]interface{}{
-				"message": "initial",
-			},
-		}).Result()
-		err = rs.rdb.XGroupCreate(ctx, rs.streamName, rs.groupName, r).Err()
-		if err != nil {
-			return
-		}
-		err = rs.rdb.XDel(ctx, rs.streamName, r).Err()
+	r, e := rs.rdb.XInfoGroups(ctx, rs.streamName).Result()
+	if e != nil || len(r) == 0 {
+		err = rs.rdb.XGroupCreateMkStream(ctx, rs.streamName, rs.groupName, "0").Err()
 		if err != nil {
 			return
 		}
@@ -86,7 +78,18 @@ func (rs *RedisStream) ReadGroup(ctx context.Context) (
 	if err != nil {
 		return
 	}
-	rsr = re[0].Messages[0].Values
+	if len(re) == 0 || len(re[0].Messages) == 0 {
+		err = fmt.Errorf("no messages found")
+		return
+	}
+
+	message := re[0].Messages[0]
+	rsr = message.Values
+
+	err = rs.rdb.XDel(ctx, rs.streamName, message.ID).Err()
+	if err != nil {
+		return
+	}
 	return
 }
 
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package version
 
-const Version = "2.15.6"
+const Version = "2.15.7"
